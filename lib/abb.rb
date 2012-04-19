f6af270ce706b6134d9b42db6651f5c8148489bb
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-04-19 22:47:37 +0900

    modify a innner word suffix to major in English

--- lib/abb.rb
+++ lib/abb.rb
@@ -62,9 +62,9 @@ module Abb
   
   module Interface
 
-    Abb.methods(false).each do |convertor|
-      define_method convertor do
-        Abb.__send__ convertor, to_str
+    Abb.methods(false).each do |converter|
+      define_method converter do
+        Abb.__send__ converter, to_str
       end
     end
   
